d0b5adcb7223d4d09cfdf1535e661e0ab48e0fe6
lavender-snow <lavender32192@gmail.com>
2021-10-01 14:42:22 +0900

    ドレミ表記追加

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
 const scales = ['C8','B7','A#7','A7','G#7','G7','F#7','F7','E7','D#7','D7','C#7','C7','B6','A#6','A6','G#6','G6','F#6','F6','E6','D#6','D6','C#6','C6','B5','A#5','A5','G#5','G5','F#5','F5','E5','D#5','D5','C#5','C5','B4','A#4','A4','G#4','G4','F#4','F4','E4','D#4','D4','C#4','C4','B3','A#3','A3','G#3','G3','F#3','F3','E3','D#3','D3','C#3','C3','B2','A#2','A2','G#2','G2','F#2','F2','E2','D#2','D2','C#2','C2','B1','A#1','A1','G#1','G1','F#1','F1','E1','D#1','D1','C#1','C1','B0','A#0','A0']
+const scales_en = ["C","D","E","F","G","A","B"]
+const scales_jp = ["ド","レ","ミ","ファ","ソ","ラ","シ"]
 
 document.addEventListener('DOMContentLoaded', function(e) {
     addColumn(1);
@@ -12,11 +14,6 @@ document.addEventListener('DOMContentLoaded', function(e) {
         maxScaleRange.appendChild(option);
     }
 
-    maxScaleRange.addEventListener('change', function(e) {
-        changeMinScale(maxScaleRange.selectedIndex)
-        changeTable();
-    });
-
     let minScaleRange = document.getElementById("min-scale-range");
 
     minScaleRange.addEventListener('change', function(e){
@@ -25,11 +22,31 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
     maxScaleRange.selectedIndex = scales.indexOf("A5");
     changeMinScale(scales.indexOf("A5"));
+    minScaleRange.selectedIndex = 20;
+
+    maxScaleRange.addEventListener('change', function(e) {
+        let maxScaleSelectedIndex = maxScaleRange.selectedIndex;
+        let minScaleRange = document.getElementById("min-scale-range");
+        if (maxScaleSelectedIndex >= scales.indexOf(minScaleRange.options[minScaleRange.selectedIndex].value)){
+            changeMinScale(maxScaleSelectedIndex)
+        }
+
+        changeTable();
+    });
+
+    let scaleLabelsSwitch = document.getElementById("scale-labels-switch");
+    scaleLabelsSwitch.addEventListener('change', function(e) {
+        if (scaleLabelsSwitch.checked) {
+            changeScalesLabel(scales_en, scales_jp);
+        } else {
+            changeScalesLabel(scales_jp, scales_en);
+        }
+    });
+
     changeTable();
 });
 
 function changeMinScale(maxScaleSelectedIndex){
-
     let minScaleRange = document.getElementById("min-scale-range");
 
     while (minScaleRange.firstChild) {
@@ -42,7 +59,7 @@ function changeMinScale(maxScaleSelectedIndex){
         option.text = scales[i];
         minScaleRange.appendChild(option);
     }
-    minScaleRange.selectedIndex = Math.min(20,scales.length-maxScaleSelectedIndex-2);
+    minScaleRange.selectedIndex = 0;
 }
 
 function addColumn(index) {
@@ -89,5 +106,16 @@ function changeTable(){
             scoreTable.rows[i].style.display = "none";
         }
 
+    }
+}
+
+function changeScalesLabel(beforeArr, afterArr){
+    let scaleTable = document.getElementById("scale-table");
+    for (let i = 0; i < scaleTable.rows.length; i++){
+        scaleTable.rows[i].innerHTML = beforeArr.reduce((a, c, i) => {
+            const reg = new RegExp(beforeArr[i], 'gm')
+            c = a.replace(reg, afterArr[i])
+            return c
+        }, scaleTable.rows[i].innerHTML)
     }
 }
